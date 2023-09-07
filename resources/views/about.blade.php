b84781c2907b65bb92dedcb4325b85b5bc7a93ae
Sheharyar Khalid <sheharyarkhalid0313@gmail.com>
2023-09-07 06:52:14 -0700

    inner pages

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,42 +4,112 @@
 
 @section('content')
 
-<div class="degital_marketing_home_section_two">
+<section class="inner_banner_section">
     <div class="custom_container">
-        <div class="marketing_home_section_two_content_box">
-            <figure><img class="lazy" data-src="images/one.png" src="images/one.png" /></figure>
-            <div class="marketing_home_section_two_content"><small>about us</small>
-                <h2>3X Services<span class="heading_dot_color">.</span></h2>
-                <p>We are <span class="inner_detail_content_two">experts in software development & marketing.</span> We help you grow your online sales through various channels. What would it be like to have a team of experts working for you?</p>
-              
+        <div class="inner_banner_box">
+            <div class="inner_banner_figcaption">
+                <h4>
+                    Experience the
+                    Power of 3xservices
+                   
+
+                </h4>
+                <p> 3xservices is a software development company that provides
+                    quality web applications, mobile applications, UI/UX designs, and more
+                    - all at an unbeatable price.</p>
             </div>
+            <figure><img src="images/mobile-website-img.png"></figure>
         </div>
     </div>
-</div>
+</section>
 
 
-<div class="home_custom_section_8_main">
+<div class="about_page_section_1_main">
     <div class="custom_container">
-    <div class="home_custom_section_8">
-        <figure><img src="images/ipad.png"></figure>
-        <div class="home_custom_section_8_body_main">
-            <div class="home_custom_section_8_body">
-                <div class="home_custom_section_8_heading">
-                    <h3>Empowering Your Business Growth through Innovative Digital Marketing Solutions</h3>
-                    
-                    <p>At 3X Services, we believe that every business deserves a digital marketing partner that is committed to achieving their online goals. We are a full-service digital marketing agency dedicated to providing our clients with result-driven solutions that are tailored to their unique needs.</p>
-
-                    <p>With our team of experienced and skilled professionals, we have successfully helped businesses of all sizes, across various industries, to grow and achieve their objectives. We pride ourselves on our ability to stay ahead of the curve in the ever-changing world of digital marketing, leveraging the latest tools and strategies to help our clients achieve maximum results.</p>
-
-                    <p>Our services include a wide range of digital marketing solutions, including search engine optimization, social media marketing, pay-per-click advertising, content marketing, email marketing, and more. We work closely with our clients to develop a comprehensive strategy that aligns with their business objectives and budget, ensuring that they achieve the best possible ROI</p>
-
-                    </p>
+    <div class="about_page_section_1">
+        <div class="experience_box_main">
+        <div class="experience_box">
+            <h4>4+</h4>
+            <small>Year’s of experience in
+                this field.</small>
+        </div>
+        </div>
+                <div class="about_page_section_1_content">
+                    <h3> About 3xservices</h3>
+                   
+                  <p>3xservices has been providing web solutions since 2019.
+                     We have a team of professionals and tech experts who are continuously providing a wide range of website designs and development services.
+                      We provide clients with optimal digital solutions because we thrive to explore techniques and solutions that maximise the business value, 
+                      consistent growth, and sustainability for continued success. From creating web development solutions and responsive website designs to building 
+                      custom e-commerce experiences, using the most modern web technologies. We also offer hosting services, domain registration, web development in personal 
+                      and business divisions. At 3xservices, we focus on high quality and timely delivery of our services. We provide cost effective and customised solutions. 
+                      Whatever our clients’ project requires, 3xservices has the tools and experience to ensure that their goals are fulfilled. We laid down the foundation of a 
+                      disciplined and receptive work environment. </p>
+                  
                 </div>
-             
-            </div>
-        </div>
+
     </div>
 </div>
 </div>
+
+
+
+<section class="about_page_section_2">
+    <div class="custom_container">
+    
+    <div class="about_page_section_2_box">
+    <div class="about_page_section_2_box_data">
+    
+    <div class="about_page_section_2_heading">
+    <h4>Mission
+       </h4>
+    
+    </div>
+    <div class="about_page_section_2_discription">
+    <p> Our mission is to be a well known cost-competitive and functional solution provider in the IT sector. We aim to provide a growing mindset, instil flexibility that leads to a diversified range of unique client-centric solutions.</p>
+    </div>
+    </div>
+    <div class="about_page_section_2_box_data">
+    
+    <div class="about_page_section_2_heading">
+    <h4>Vision
+       </h4>
+    </div>
+    <div class="about_page_section_2_discription">
+    <p> We aspire to be the most reliable and innovative service provider of web solutions and designing. Our key for achieving the ultimate goal of success is to emerge as a globally recognized company by providing superior solutions.</p>
+    </div>
+    </div>
+    <div class="about_page_section_2_box_data">
+    
+    <div class="about_page_section_2_heading">
+    <h4>Value
+       </h4>
+    </div>
+    <div class="about_page_section_2_discription">
+    <p> The DNA of our organisation for a growing mindset that ultimately produces unique solutions are respect, simplicity and result-oriented.</p>
+    </div>
+    </div>
+    <div class="about_page_section_2_box_data">
+    
+    <div class="about_page_section_2_heading">
+    <h4>Goal
+       </h4>
+    </div>
+    <div class="about_page_section_2_discription">
+    <p> We stick to a simple goal in every project; providing the most reliable and feasible software solutions within the budget.</p>
+    </div>
+    </div>
+    
+    
+    
+    
+    </div>
+    </div>
+</section>
+
+@include('include/contact_section')
+ 
+@include('include/plan_section')
+
 
 @endsection
--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -3,74 +3,52 @@
 
 @section('content')
 
-<div class="portfolio_custom_section_1_main">
+<section class="inner_banner_section">
     <div class="custom_container">
-
-
-        <div class="portfolio_custom_section_1_body">
-            <div class="portfolio_custom_section_1_heading">
-                <h3>Our Projects</h3>
-                <p>We have an experienced team of production and inspection personnel to ensure quality.</p>
+        <div class="inner_banner_box">
+            <div class="inner_banner_figcaption">
+                <h4>
+                    Portfolio
+                </h4>
+                <p> Clients come to us with a vision - we help them build it. Our successful record
+                    of client portfolio proves that we deliver quality and ensure to take their business
+                    to the next level.</p>
             </div>
-            <!-- <ul class="nav nav-tabs meals_tab_main" id="myTab" role="tablist">
-                <li class="nav-item"><a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">All</a></li>
-                <li class="nav-item"><a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Mobile App</a></li>
-                <li class="nav-item"><a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Real Estate</a></li>
-            </ul> -->
-            <!-- <div class="tab-content meal_tab_data_main" id="myTabContent">
-                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-
-                    <section class="home_section_one">
-                        
-
-                            <div class="home_section_one_box">
-                                <div class="home_section_one_single_box_main">
-                                <a class="home_section_one_single_box">
-                                    <div class="section_one_single_box_icon">
-                                        <img src="images/pt1.jpg">
-                                    </div>
-                                    <div class="section_one_single_box_heading">
-                                        <h4>Commission free system</h4>
-                                    </div>
-                                    <div class="section_one_single_box_discription">
-                                        <p>With our platform, you don't have to worry about paying commission fees on your sales. This means you get to keep more of your profits, which can help your business grow.</p>
-                                    </div>
-                                </a>
-                            </div>
-                            </div>
-                      
-                    </section>
+            <figure><img src="images/mobile-website-img.png"></figure>
+        </div>
+    </div>
+</section>
 
 
 
-                </div>
-                <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+<div class="degital_marketing_home_section_one" >
+    <div class="custom_container">
+        <div class="marketing_home_section_one_content_box">
+            <div class="marketing_home_section_one_content">
+                <h2> Our Portfolio</span></h2>
+                <p> 3xservices is here to provide you with high-quality web, mobile app, and UI/UX services. We create apps
+                    that are tailored to your needs and suits your unique vision.</p>
+               
+               
+            </div>
+        </div>
+    </div>
+</div>
 
-                </div>
-                <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
 
-                </div>
-            </div> -->
-            <div class="degital_marketing_home_section_seven">
-            <div class="custom_video_container">
+<div class="degital_marketing_home_section_seven" style="margin-bottom: 100px">
+    <div class="custom_video_container">
 
-<div class="video_section_thumbnail">
-   
-        <div class="home_banner_slider">
+        <div class="video_section_thumbnail">
+
+            <div class="home_banner_slider">
                 <div><img class="lazy" data-src="images/3x1.png" src="images/3x1.png" /></div>
                 <div><img class="lazy" data-src="images/3x2.png" src="images/3x2.png" /></div>
                 <div><img class="lazy" data-src="images/slider3.png" src="images/slider3.png" /></div>
             </div>
-        <div class="home_banner_slider-dots"></div>
-    <div class="thumbnail_custom_extra_image"></div>
-</div>
-</div>
-</div>
-
-            <div class="portfolio_custom_section_1_shop_btn"><a href="/contact.php">Contact Us</a></div>
-
+            <div class="home_banner_slider-dots"></div>
+            <div class="thumbnail_custom_extra_image"></div>
         </div>
-
     </div>
 </div>
 
